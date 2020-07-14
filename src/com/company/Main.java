abeb9e05ad472f195881598ecbb3e324d8db5b29
ермек <ermek@MacBook-Pro-ermek.local>
2020-07-14 16:40:38 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printWords();
+        printWords();
+        printWords();
+        printWords();
+        printWords();
+        
+
+    }
+
+
+    // write your code here
+
+
+    public static void printWords() {
+        System.out.println("Hello");
+        System.out.println("My");
+        System.out.println("Name");
+        System.out.println("Is");
+        System.out.println("Ermek");
+
+    }
+
+}
+
